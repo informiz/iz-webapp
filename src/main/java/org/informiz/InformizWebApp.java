c986cdc60011a508b86d7415b2135da1d2ffe023
Nira Amit <amit@datlinq.com>
2020-07-31 20:50:35 +0200

    Customize embedded tomcat - testing

--- src/main/java/org/informiz/InformizWebApp.java
+++ src/main/java/org/informiz/InformizWebApp.java
@@ -1,15 +1,11 @@
 package org.informiz;
 
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.servlet.ServletContext;
@@ -39,25 +35,6 @@ public class InformizWebApp extends SpringBootServletInitializer {
         // TODO: better configure here or in application.properties? I.e:
         // server.servlet.session.cookie.http-only=true
         // server.servlet.session.cookie.secure=true
-
-    }
-
-    //Tomcat large file upload connection reset
-    //http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
-    @Bean
-    public TomcatServletWebServerFactory containerFactory() {
-        return new TomcatServletWebServerFactory() {
-            protected void customizeConnector(Connector connector) {
-                super.customizeConnector(connector);
-                connector.setMaxPostSize(maxUploadSizeInMb);
-                connector.setMaxSavePostSize(maxUploadSizeInMb);
-                if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
-
-                    ((AbstractHttp11Protocol <?>) connector.getProtocolHandler()).setMaxSwallowSize(maxUploadSizeInMb);
-                    logger.info("Set MaxSwallowSize "+ maxUploadSizeInMb);
-                }
-            }
-        };
 
     }
 
