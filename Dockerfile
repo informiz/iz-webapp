2bdd4cd7ad123ae6bc6e4ba065f1d39b004b7838
Nira Amit <amit@datlinq.com>
2020-08-20 17:23:53 +0200

    Automated deployment (partial)

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# TODO: official linux+java8 image that does not conflict with IBM/Spring grpc and netty libs?
+FROM rtfpessoa/ubuntu-jdk8
+
+RUN groupadd spring && useradd -g spring spring
+USER spring:spring
+
+ARG DEPENDENCY=target/dependency
+COPY ${DEPENDENCY}/BOOT-INF/lib /app/lib
+COPY ${DEPENDENCY}/META-INF /app/META-INF
+COPY ${DEPENDENCY}/BOOT-INF/classes /app
+
+ENV profile test
+ENV clientId overrideMe
+ENV clientSecret overrideMe
+ENV dbUser overrideMe
+ENV dbPass overrideMe
+# TODO: is this logged somewhere?? More secure way to pass the secrets from k8s/cloudbuild?
+ENTRYPOINT ["java", "-Djava.security.egd=file:/dev/./urandom","-Dspring.profiles.active=${profile}", \
+"-Dspring.security.oauth2.client.registration.google.client-id=${clientId}", \
+"-Dspring.security.oauth2.client.registration.google.client-secret=${clientSecret}", \
+"-Dspring.datasource.username=${dbUser}", \
+"-Dspring.datasource.password=${dbPass}", \
+"-cp","app:app/lib/*","org.informiz.InformizWebApp"]
---
+++ deploy/k8s/setup-channel.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+# TODO: can get cert and key directly from secret-manager with $(gcloud secrets versions access latest --secret=...)?
+openssl req -x509 -new -nodes -key devCAEncrypted.key -sha256 -days 3650 -out devCA.pem
+
+openssl rsa -in devCAEncrypted.key -out devCA.key
+
+kubectl create secret tls demo-channel-tls --cert devCA.pem --key devCA.key
+
+kubectl create secret generic demo-channel-service-account-creds --from-file=key.json=/path/to/channel-service-key.json
+
+kubectl create secret generic demo-channel-db-creds --from-literal=username=test --from-literal=password=test
+
+kubectl create secret generic demo-channel-oauth-creds --from-literal=client=<CLIENT_ID> --from-literal=secret=<CLINET_SERVICE>
+
+
+
+sed -i "s/<PROFILE>/test/g" deploy-test/iz-deployment.yaml
+sed -i "s/<CHANNEL_NAME>/demo-channel/g" deploy-test/*.yaml
+
+
+kubectl apply -f iz-deployment.yaml
+
+kubectl apply -f iz-webapp-service.yaml
+
+kubectl apply -f iz-webapp-ingress.yaml
+
