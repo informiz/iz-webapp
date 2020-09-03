a0602c49b1eb6c30f295650e36c4f7c0acb15cb2
Nira Amit <amit@datlinq.com>
2020-09-03 18:35:06 +0200

    Code cleanup

--- src/main/java/org/informiz/conf/SecurityConfig.java
+++ src/main/java/org/informiz/conf/SecurityConfig.java
@@ -1,42 +1,27 @@
 package org.informiz.conf;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
-import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContexts;
 import org.informiz.auth.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.firewall.HttpFirewall;
-import org.springframework.security.web.firewall.StrictHttpFirewall;
-import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.SSLContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 
 import static org.informiz.auth.CookieRequestCache.CACHE_REQUEST_COOKIE_NAME;
 import static org.informiz.auth.CookieUtils.JWT_COOKIE_NAME;
@@ -136,43 +121,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 CookieUtils.setCookie(response, CACHE_REQUEST_COOKIE_NAME, 0, "");
             }
         };
-    }
-
-    @Value("${server.ssl.key-store}")
-    private Resource keyStore;
-
-    @Value("${server.ssl.key-store-password}")
-    private String keyStorePassword;
-
-    @Value("${server.ssl.trust-store}")
-    private Resource trustStore;
-
-    @Value("${server.ssl.trust-store-password}")
-    private String trustStorePassword;
-
-    private RestTemplate getRestTemplate() {
-        try {
-            SSLContext sslContext = SSLContexts.custom()
-                    .loadKeyMaterial(
-                            keyStore.getFile(),
-                            keyStorePassword.toCharArray(),
-                            keyStorePassword.toCharArray())
-                    .loadTrustMaterial(
-                            trustStore.getURL(),
-                            keyStorePassword.toCharArray(),
-// TODO: ******************************** DEVELOPING, REMOVE THIS!! ********************************
-                            new TrustSelfSignedStrategy())
-                    .build();
-
-            HttpClient httpClient = HttpClients.custom()
-// TODO: ************************ DEVELOPING, REMOVE NoopHostnameVerifier!! ************************
-                    // use NoopHostnameVerifier with caution, see https://stackoverflow.com/a/22901289/3890673
-                    .setSSLSocketFactory(new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier()))
-                    .build();
-
-            return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
-        } catch (IOException | GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
