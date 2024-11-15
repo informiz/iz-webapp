d6e0844bde0033c950365943ab93678d0db7c179
Shay Goldin <goldinshay@gmail.com>
2024-11-15 12:49:44 +0100

    Add Pub/Sub publisher functionality

--- src/main/java/org/informiz/ctrl/citation/CitationController.java
+++ src/main/java/org/informiz/ctrl/citation/CitationController.java
@@ -1,6 +1,7 @@
 package org.informiz.ctrl.citation;
 
 import jakarta.validation.Valid;
+import org.informiz.conf.PubSubPublisher;
 import org.informiz.ctrl.entity.ChaincodeEntityController;
 import org.informiz.model.CitationBase;
 import org.informiz.model.InformizEntity;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = CitationController.PREFIX)
 @Validated
 public class CitationController extends ChaincodeEntityController<CitationBase> {
+    PubSubPublisher publisher = new PubSubPublisher();
 
     public static final String PREFIX = "/citation";
     public static final String EDIT_PAGE_TEMPLATE = String.format("%s/update-citation.html", PREFIX);
@@ -71,6 +73,10 @@ public class CitationController extends ChaincodeEntityController<CitationBase> 
             return String.format("%s/add-citation.html", PREFIX);
         }
         entityRepo.save(citation);
+
+// Send a message to 'hello-pubsub' topic
+        publisher.publishMessage("hello-pubsub", "Hello, Pub/Sub!");
+
         return String.format("redirect:%s/all", PREFIX);
     }
 
